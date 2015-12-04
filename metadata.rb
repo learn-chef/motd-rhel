a8bdab2c73cd6945d7c586f1bb23e796b1f2adb5
tpetchel <tpetchel@gmail.com>
2015-12-04 13:12:09 -0500

    draft of lesson code

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'motd-rhel'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures motd-rhel'
+long_description 'Installs/Configures motd-rhel'
+version '0.1.0'
