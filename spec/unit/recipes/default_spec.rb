f7fb0b7ec77bdc2bc1c9840147cdc0d8aa4870cb
tpetchel <tpetchel@gmail.com>
2016-04-01 10:41:11 -0400

    Replace hyphen with underscore

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,12 +1,12 @@
 #
-# Cookbook Name:: motd-rhel
+# Cookbook Name:: motd_rhel
 # Spec:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'motd-rhel::default' do
+describe 'motd_rhel::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
